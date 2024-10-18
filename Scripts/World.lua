737b90d8086e1ee2ab48eea580f56d206b4ef9fe
Woukie <lucaseldon@live.com>
2024-10-18 20:10:48 +0100

    Added water from survival (no rendering)

--- Scripts/World.lua
+++ Scripts/World.lua
@@ -1,4 +1,5 @@
 dofile("$CONTENT_DATA/Scripts/managers/ForceManager.lua")
+dofile("$CONTENT_DATA/Scripts/managers/WaterManager.lua")
 
 World = class( nil )
 World.terrainScript = "$CONTENT_DATA/Scripts/terrain.lua"
@@ -15,6 +16,9 @@ function World.server_onCreate(self)
 
   self.forceManager = ForceManager()
 	self.forceManager:server_onCreate(self)
+
+  self.waterManager = WaterManager()
+	self.waterManager:sv_onCreate(self)
 end
 
 function World.client_onCreate(self)
@@ -25,24 +29,43 @@ function World.client_onCreate(self)
 		self.forceManager = ForceManager()
 	end
 	self.forceManager:client_onCreate()
+
+  if self.waterManager == nil then
+		assert( not sm.isHost )
+		self.waterManager = WaterManager()
+	end
+	self.waterManager:cl_onCreate()
+end
+
+function World.server_onFixedUpdate(self)
+	self.waterManager:sv_onFixedUpdate()
+end
+
+function World.client_onFixedUpdate(self)
+  self.waterManager:cl_onFixedUpdate()
 end
 
 function World.server_onCellCreated(self, x, y)
   self.forceManager:server_onCellLoaded(x, y)
+  self.waterManager:sv_onCellLoaded(x, y)
 end
 
 function World.client_onCellLoaded(self, x, y)
   self.forceManager:client_onCellLoaded(x, y)
+  self.waterManager:cl_onCellLoaded(x, y)
 end
 
 function World.server_onCellLoaded(self, x, y)
   self.forceManager:server_onCellReloaded(x, y)
+  self.waterManager:sv_onCellReloaded(x, y)
 end
 
 function World.server_onCellUnloaded(self, x, y)
   self.forceManager:server_onCellUnloaded(x, y)
+  self.waterManager:sv_onCellUnloaded(x, y)
 end
 
 function World.client_onCellUnloaded(self, x, y)
   self.forceManager:client_onCellUnloaded(x, y)
+  self.waterManager:cl_onCellUnloaded(x, y)
 end
