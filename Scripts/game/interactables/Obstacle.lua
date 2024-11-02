34ed8d8c97259546a7cedbdf24a2b2651777bb89
Woukie <lucaseldon@live.com>
2024-11-02 13:30:35 +0000

    Updated obstacle behaviour

--- Scripts/game/interactables/Obstacle.lua
+++ Scripts/game/interactables/Obstacle.lua
@@ -1,8 +1,30 @@
 Obstacle = class(nil)
 
 function Obstacle.server_onCollision(self, other, collisionPosition, selfPointVelocity, otherPointVelocity, collisionNormal)
+  if not self.health then
+    self.health = self.data.health
+  end
+
 	if type(other) == "Shape" and sm.exists(other) then
-    other:destroyBlock(other:getClosestBlockLocalPosition(collisionPosition), sm.vec3.new(self.data.sizeX, self.data.sizeY, self.data.sizeZ))
-    self.shape:destroyShape()
+    local damage = math.random(self.data.attackLevelMin, self.data.attackLevelMax)
+    if other.isBlock then
+      other:destroyBlock(other:getClosestBlockLocalPosition(collisionPosition), sm.vec3.one(), damage)
+    else
+      other:destroyShape(damage)
+    end
+
+    if self.damageEffect then
+      sm.effect.playEffect(self.damageEffect, self.shape.worldPosition, sm.vec3.new( 0, 0, 0 ), self.shape.worldRotation)
+    end
+
+    self.health = self.health - 1
+    if self.health <= 0 then
+      if self.destroyEffect then
+        sm.effect.playEffect(self.destroyEffect, self.shape.worldPosition, sm.vec3.new( 0, 0, 0 ), self.shape.worldRotation)
+      end
+      self.shape:destroyShape()
+    end
+  elseif type(other) == "Character" then
+    other:setDowned(true)
 	end
 end
