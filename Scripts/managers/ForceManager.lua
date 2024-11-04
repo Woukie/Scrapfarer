49bb64db4ce306c84c3f746c1e2100ac9914e42c
Woukie <lucaseldon@live.com>
2024-11-04 21:39:18 +0000

    Fixed force averaging

--- Scripts/managers/ForceManager.lua
+++ Scripts/managers/ForceManager.lua
@@ -8,14 +8,6 @@ exclusions[tostring(obj_rock_obstacle_01)] = true
 exclusions[tostring(obj_rock_obstacle_02)] = true
 exclusions[tostring(obj_log_obstacle_01)] = true
 
-local function addRequestedForce(self, target, force)
-  if not self.forceRequests[target] then
-    self.forceRequests[target] = {}
-  end
-
-  table.insert(self.forceRequests[target], force)
-end
-
 function ForceManager:onCreate()
 	self.cells = {}
   self.forceRequests = {}
@@ -34,16 +26,16 @@ function ForceManager:client_onCreate()
 end
 
 function ForceManager:server_onFixedUpdate()
-	for target, forces in pairs(self.forceRequests) do
+	for _, request in pairs(self.forceRequests) do
+    local target = request.instance
     local type = type(target)
 
     if sm.exists(target) then
       local force = sm.vec3.zero()
-      for _, forceRequest in ipairs(forces) do
+      for _, forceRequest in ipairs(request.forces) do
         force = force + forceRequest
       end
-
-      force = force / #forces
+      force = force / #request.forces
 
       if type == "Character" then
         ApplyCharacterImpulse(target, force, force:length())
@@ -124,20 +116,36 @@ function ForceManager:trigger_onStay(trigger, results)
 	assert(params)
 
   for _, result in ipairs(results) do
-    if type(result) == "Character" then
+    local force
+
+    local type = type(result)
+    if type == "Character" then
       if result:isTumbling() then
-        addRequestedForce(self, result, params.force / 6)
+        force = params.force / 6
       else
         -- Only apply force if fully in force field (matches behaviour of water)
         local characterFloatOffset = 0.2 + ( result:isCrouching() and 0.4 or 0.0 )
         local characterFloatHeight = result.worldPosition.z + characterFloatOffset
 
         if trigger:getWorldMax().z > characterFloatHeight then
-          addRequestedForce(self, result, params.force)
+          force = params.force
         end
       end
-    else
-      addRequestedForce(self, result, params.force)
+    elseif type == "Body" then
+      force = params.force
+    end
+
+    if force then
+      -- Two of the same results do not equate, so we index by Id and type
+      local requestId = type..result:getId()
+      if self.forceRequests[requestId] then
+        table.insert(self.forceRequests[requestId].forces, force)
+      else
+        self.forceRequests[requestId] = {
+          instance = result,
+          forces = {force}
+        }
+      end
     end
   end
 end
