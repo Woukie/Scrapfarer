11259b181e6d2bbc8d04c4d53e785ae673fbfded
Woukie <lucaseldon@live.com>
2024-11-05 19:08:14 +0000

    Implemented start button logic

---
+++ Scripts/game/interactables/StartButton.lua
@@ -0,0 +1,19 @@
+StartButton = class(nil)
+
+StartButton.poseWeightCount = 1
+
+function StartButton:server_setState(params)
+  if params.state then
+    sm.effect.playEffect("Elevator Button", self.shape:getWorldPosition())
+    g_serverGameManager:startRun(params.character:getPlayer())
+  end
+end
+
+function StartButton.client_onInteract(self, character, state)
+  self.network:sendToServer("server_setState", {state = state, character = character})
+  if state then
+    self.interactable:setPoseWeight(0, 1)
+  else
+    self.interactable:setPoseWeight(0, 0)
+  end
+end
