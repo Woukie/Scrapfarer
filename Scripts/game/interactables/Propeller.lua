312b94e8b4ac6214b5979155291128d5d367f22e
Woukie <lucaseldon@live.com>
2024-11-13 18:36:49 +0000

    Added water resistance to propellors

--- Scripts/game/interactables/Propeller.lua
+++ Scripts/game/interactables/Propeller.lua
@@ -9,23 +9,28 @@ function Propeller:server_onCreate()
 end
 
 function Propeller:server_onFixedUpdate()
-  local shape = self.interactable:getShape()
-  local body = shape:getBody()
-  local getAngularVelocity = body:getAngularVelocity()
-
   for _, result in ipairs(self.areaTrigger:getContents()) do
     if sm.exists( result ) then
       if type( result ) == "AreaTrigger" then
         local userData = result:getUserData()
         if userData and userData.water then
-          local force = -(sm.quat.inverse(shape:getWorldRotation()) * getAngularVelocity).y * self.data.force
-
+          local shape = self.interactable:getShape()
+          local body = shape:getBody()
+          local angularVelocity = (sm.quat.inverse(shape:getWorldRotation()) * body:getAngularVelocity()).y
+          local force = -angularVelocity * self.data.force
           sm.physics.applyImpulse(shape, shape:getZAxis() * force)
-          return
+
+          -- Max resistance from one engine is 24ish
+          local resistance = angularVelocity / 3
+          resistance = math.min(50, math.max(-50, resistance))
+
+          sm.physics.applyTorque(
+            body,
+            sm.vec3.new(-resistance, 0, 0)
+          )
+          break
         end
       end
     end
   end
-
-  sm.physics.applyTorque(body, getAngularVelocity / -3)
 end
